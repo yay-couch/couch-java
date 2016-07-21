6e93b88a52490f275d5fe493294c0c60320bbe08
Kerem Güneş <k-gun@mail.com>
2016-07-21 23:21:23 +0300

    Add map().

--- couch/util/Util.java
+++ couch/util/Util.java
@@ -1,9 +1,23 @@
 package couch.util;
 
+import java.util.HashMap;
+import java.lang.IllegalArgumentException;
 import javax.xml.bind.DatatypeConverter;
 
 public final class Util
 {
+    public static HashMap map(Object... args) throws IllegalArgumentException {
+        int argc = args.length;
+        if (argc % 2 != 0) {
+            throw new IllegalArgumentException("Wrong args count!");
+        }
+        HashMap map = new HashMap<String, Object>();
+        for (int i = 1; i < argc; i += 2) {
+            map.put((String) args[i - 1], args[i]);
+        }
+        return map;
+    }
+
     public static String base64Encode(String data)
     {
         return DatatypeConverter.printBase64Binary(data.getBytes());
