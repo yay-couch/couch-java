132d3e0f0f58e5edb7ea590decee04626f2b861e
Kerem Güneş <k-gun@mail.com>
2016-08-17 02:59:23 +0300

    Add setConfig() & getConfig() & constructors.

--- couch/Couch.java
+++ couch/Couch.java
@@ -2,19 +2,37 @@ package couch;
 
 import java.util.HashMap;
 
+import couch.util.Util;
+
 public class Couch
 {
     public static final String NAME = "Couch";
     public static final String VERSION = "1.0.0";
     public static Boolean DEBUG = false;
-    private HashMap config = new HashMap<String, Object>();
+    private HashMap<String, Object> config = new HashMap<String, Object>();
 
     public Couch() {}
 
-    // public Couch(HashMap config) {}
+    public Couch(HashMap<String, Object> config) {
+        this.setConfig(config);
+    }
 
-    public Couch(HashMap config, Boolean debug) {
-        this.config = config;
+    public Couch(HashMap<String, Object> config, Boolean debug) {
+        this.setConfig(config);
+
         this.DEBUG = debug;
+    }
+
+    public void setConfig(HashMap<String, Object> config) {
+        this.config = config;
+
+        Boolean debug = (Boolean) this.config.get("debug");
+        if (debug != null) {
+            this.DEBUG = debug;
+        }
+    }
+
+    public HashMap<String, Object> getConfig() {
+        return this.config;
     }
 }
