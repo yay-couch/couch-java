28bf12c67e5673397c7c7024c8bac6ec56552fe2
Kerem Güneş <k-gun@mail.com>
2016-08-17 02:06:11 +0300

    Remove final.

--- couch/Couch.java
+++ couch/Couch.java
@@ -6,7 +6,7 @@ public class Couch
 {
     public static final String NAME = "Couch";
     public static final String VERSION = "1.0.0";
-    public static final Boolean DEBUG = false;
+    public static Boolean DEBUG = false;
     private HashMap config = new HashMap<String, Object>();
 
     public Couch() {}
