d65e61175ce9b746fffff6eb4519e516eef68afb
Kerem Güneş <k-gun@mail.com>
2016-08-17 02:04:16 +0300

    Remove public's, add getters.

--- couch/Client.java
+++ couch/Client.java
@@ -6,15 +6,30 @@ import couch.http.Response;
 public class Client
 {
     private Couch couch;
-    public String host = "localhost";
-    public int port = 5984;
-    public String username;
-    public String password;
+    private String host = "localhost";
+    private int port = 5984;
+    private String username;
+    private String password;
     private Request request;
     private Response response;
 
-    public Client(Couch couch)
-    {
+    public Client(Couch couch) {
         this.couch = couch;
+    }
+
+    public String getHost() {
+        return this.host;
+    }
+
+    public int getPort() {
+        return this.port;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public String getPassword() {
+        return this.password;
     }
 }
