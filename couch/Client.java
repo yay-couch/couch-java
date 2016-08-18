c9e1f37afb35b1cdb8196cb79f8548119a546075
Kerem Güneş <k-gun@mail.com>
2016-08-18 04:20:01 +0300

    Add request().

--- couch/Client.java
+++ couch/Client.java
@@ -1,5 +1,11 @@
 package couch;
 
+import java.util.Map;
+import java.util.HashMap;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+import couch.util.Util;
 import couch.http.Request;
 import couch.http.Response;
 
@@ -43,5 +49,42 @@ public class Client
 
     public Response getResponse() {
         return this.response;
+    }
+
+    public void request(String uri, Object uriParams, Object body,
+            HashMap<String, String> headers) throws Exception {
+        Pattern pattern = Pattern.compile("^([A-Z]+)\\s+(/.*)");
+        Matcher matcher = pattern.matcher(uri);
+        if (!matcher.find()) {
+            throw new Exception("Usage: <REQUEST METHOD> <REQUEST URI>!");
+        }
+
+        this.request = new Request(this);
+        this.response = new Response();
+
+        uri = String.format("%s:%s/%s", this.host, this.port, matcher.group(2).replaceAll("\\s+|/+", ""));
+
+        this.request
+            .setMethod(matcher.group(1))
+            .setUri(uri, uriParams)
+            .setBody(body);
+
+        if (headers instanceof HashMap) {
+            for (Map.Entry header : headers.entrySet()) {
+                this.request.setHeader((String) header.getKey(), (String) header.getValue());
+            }
+        }
+
+        String result = this.request.send();
+        if (result != "") {
+            String[] tmp = result.split("\\r\\n\\r\\n", 2);
+            if (tmp.length == 2) {
+                headers = Util.parseHeaders(tmp[0]);
+                System.out.println(headers);
+            }
+
+            // System.out.println(tmp[3]);
+        }
+
     }
 }
