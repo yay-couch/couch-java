53f22cb36207b4fd4011e680b3e7cb791b80fd1a
Kerem Güneş <k-gun@mail.com>
2016-08-19 04:21:58 +0300

    Comp. http.Response object.

--- couch/http/Response.java
+++ couch/http/Response.java
@@ -1,6 +1,59 @@
 package couch.http;
 
-public class Response
+import java.util.HashMap;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+public class Response extends Stream
 {
-    //
+    private String status;
+    private Integer statusCode = 0;
+    private String statusText = "";
+
+    public Response() {
+        this.type = Stream.TYPE_RESPONSE;
+        this.httpVersion = "1.0";
+        this.headers = new HashMap<String, String>();
+    }
+
+    public void setStatus(String status) {
+        Pattern pattern = Pattern.compile("^HTTP/(\\d+\\.\\d+)\\s+(\\d+)\\s+(.+)");
+        Matcher matcher = pattern.matcher(status);
+        if (!matcher.find()) {
+            return;
+        }
+
+        this.status = status.trim();
+        this.httpVersion = matcher.group(1);
+        this.setStatusCode(Integer.valueOf(matcher.group(2)));
+        this.setStatusText(matcher.group(3));
+    }
+    public String getStatus() {
+        return this.status;
+    }
+
+    public void setStatusCode(Integer statusCode) {
+        this.statusCode = statusCode;
+    }
+    public Integer getStatusCode() {
+        return this.statusCode;
+    }
+
+    public void setStatusText(String statusText) {
+        this.statusText = statusText;
+    }
+    public String getStatusText() {
+        return this.statusText;
+    }
+
+    public void setBody(Object body) {
+        if (body != null) {
+            this.body = body;
+        }
+    }
+
+    public String toString() {
+        return super.toString(String.format(
+            "HTTP/%s %s %s\r\n", this.httpVersion, this.statusCode, this.statusText));
+    }
 }
