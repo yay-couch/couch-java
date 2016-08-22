70a893dde299004cf42d4b5234907c3d30d37071
Kerem Güneş <k-gun@mail.com>
2016-08-23 01:47:07 +0300

    Tests..

--- test.java
+++ test.java
@@ -16,7 +16,7 @@ class Test
     public static void main(String[] args) throws Exception {
         Couch couch = new Couch();
         // Couch couch = new Couch(null, true);
-        // Couch couch = new Couch(Util.map("debug", true));
+        // Couch couch = new Couch(Util.paramList("debug", true));
         Client client = new Client(couch);
         Request request = new Request(client);
 
@@ -25,13 +25,15 @@ class Test
         // request.send("");
         // System.out.println(request.toString());
 
-        Response response = client.request("GET /", null, null, null);
-        System.out.println(response.toString());
+        // Response response = client.request("GET /", null, null, null);
+        Response response = client.head("/", null, null);
+        System.out.println(client.getRequest().toString());
+        System.out.println(client.getResponse().toString());
 
         System.out.println("---");
 
         // JSONObject jo = (JSONObject) response.getBodyData();
-        // // System.out.println(jo.get("foo"));
+        // System.out.println(jo.get("couchdb"));
         // System.out.println(((JSONObject)jo.get("vendor")).get("name"));
 
         // request.setMethod("GET");
@@ -39,7 +41,7 @@ class Test
 
         // System.out.println(request.getHeaderAll());
 
-        // HashMap query = Util.map(
+        // HashMap query = Util.paramList(
         //     "foo", "bar",
         //     "x", true,
         //     "y", 1,
@@ -62,22 +64,26 @@ class Test
         // map.put("name", "foo");
         // String json = JSONValue.toJSONString(map);
 
-        // HashMap map = Util.map("name", "foo");
+        // HashMap map = Util.paramList("name", "foo");
 
-        // String js = Util.jsonEncode(map);
+        // String js = Util.toJsonString(map);
         // System.out.println(js);
 
-        // JSONObject jo = Util.jsonDecode(js);
+        // JSONObject jo = Util.toJsonObject(js);
         // System.out.println(jo.get("name"));
 
         // JSONObject js = new JSONObject(map);
         // System.out.println(js.get("name") == 1);
 
-        // JSONObject json = Util.jsonDecode("{\"foo\":123}");
+        // Json json = Util.toJsonObject("{\"foo\":123}");
         // System.out.println(json.get("foo"));
 
-        // URL url = Util.urlParse("localhost/foo");
+        // URL url = Util.parseUrl("localhost/foo");
         // System.out.println(url.getHost());
+
+        // Map p = Util.paramList("debug", 1);
+        // Map p = Util.param(new HashMap<String,Object>());
+        // System.out.println(p);
     }
 }
 
