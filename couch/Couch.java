db7d7a6b4224d65c8c5f0c9057192de12c9d9c4d
Kerem Güneş <k-gun@mail.com>
2016-08-22 03:20:41 +0300

    Boolean's.

--- couch/Couch.java
+++ couch/Couch.java
@@ -8,7 +8,7 @@ public class Couch
 {
     public static final String NAME = "Couch";
     public static final String VERSION = "1.0.0";
-    public static boolean DEBUG = false;
+    public static Boolean DEBUG = false;
     private HashMap<String, Object> config = new HashMap<String, Object>();
 
     public Couch() {}
@@ -17,7 +17,7 @@ public class Couch
         this.setConfig(config);
     }
 
-    public Couch(HashMap<String, Object> config, boolean debug) {
+    public Couch(HashMap<String, Object> config, Boolean debug) {
         this.setConfig(config);
 
         this.DEBUG = debug;
@@ -30,8 +30,8 @@ public class Couch
 
         this.config = config;
 
-        boolean debug = (boolean) this.config.get("debug");
-        if (debug) {
+        Boolean debug = (Boolean) this.config.get("debug");
+        if (debug != null) {
             this.DEBUG = debug;
         }
     }
