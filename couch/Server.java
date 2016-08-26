ddf64c25065f0693e567706048f321f58d3b11bf
Kerem Güneş <k-gun@mail.com>
2016-08-26 10:41:28 +0300

    Remove null's.

--- couch/Server.java
+++ couch/Server.java
@@ -23,11 +23,11 @@ public class Server
     }
 
     public boolean ping() throws Exception {
-        return (200 == this.client.head("/", null, null).getStatusCode());
+        return (200 == this.client.head("/").getStatusCode());
     }
 
     public JSONObject info() throws Exception {
-        return this.client.get("/", null, null).getBodyData().jsonObject();
+        return this.client.get("/").getBodyData().jsonObject();
     }
 
     public String version() throws Exception {
@@ -35,32 +35,32 @@ public class Server
     }
 
     public JSONArray getActiveTasks() throws Exception {
-        return this.client.get("/_active_tasks", null, null).getBodyData().jsonArray();
+        return this.client.get("/_active_tasks").getBodyData().jsonArray();
     }
 
     public JSONArray getAllDatabases() throws Exception {
-        return this.client.get("/_all_dbs", null, null).getBodyData().jsonArray();
+        return this.client.get("/_all_dbs").getBodyData().jsonArray();
     }
 
     public JSONObject getDatabaseUpdates() throws Exception {
         return this.getDatabaseUpdates(null);
     }
     public JSONObject getDatabaseUpdates(Object query) throws Exception {
-        return this.client.get("/_db_updates", query, null).getBodyData().jsonObject();
+        return this.client.get("/_db_updates", query).getBodyData().jsonObject();
     }
 
     public String getLogs() throws Exception {
         return this.getLogs(null);
     }
     public String getLogs(Object query) throws Exception {
-        return (String) this.client.get("/_log", query, null).getBody();
+        return (String) this.client.get("/_log", query).getBody();
     }
 
     public JSONObject getStats() throws Exception {
         return this.getStats("");
     }
     public JSONObject getStats(String path) throws Exception {
-        return this.client.get("/_stats/"+ path, null, null).getBodyData().jsonObject();
+        return this.client.get("/_stats/"+ path).getBodyData().jsonObject();
     }
 
     public String getUuid() {
@@ -74,7 +74,7 @@ public class Server
     public String[] getUuids(int count) throws Exception {
         Map query = Util.paramList("count", count);
         String[] uuids = new String[count];
-        JSONObject data = this.client.get("/_uuids", query, null).getBodyData().jsonObject();
+        JSONObject data = this.client.get("/_uuids", query).getBodyData().jsonObject();
         if (data.has("uuids")) {
             List<Object> uuidsList = ((JSONArray) data.get("uuids")).toList();
             for (int i = 0; i < uuidsList.size(); i++) {
@@ -89,11 +89,11 @@ public class Server
         if (body.get("source") == null || body.get("target") == null) {
             throw new Exception("Both source & target required!");
         }
-        return this.client.post("/_replicate", null, body, null).getBodyData().jsonObject();
+        return this.client.post("/_replicate", null, body).getBodyData().jsonObject();
     }
 
     public boolean restart() throws Exception {
-       return (202 == this.client.post("/_restart", null, null, null).getStatusCode());
+       return (202 == this.client.post("/_restart").getStatusCode());
     }
 
     public JSONObject getConfig() throws Exception {
@@ -103,14 +103,14 @@ public class Server
         return this.getConfig(section, "");
     }
     public JSONObject getConfig(String section, String key) throws Exception {
-        return this.client.get("/_config/"+ section +"/"+ key, null, null).getBodyData().jsonObject();
+        return this.client.get("/_config/"+ section +"/"+ key).getBodyData().jsonObject();
     }
 
     public String setConfig(String section, String key, String value) throws Exception {
         if (Util.isEmpty(section) || Util.isEmpty(key)) {
             throw new Exception("Both section & key required!");
         }
-        return (String) this.client.put("/_config/"+ section +"/"+ key, null, value, null).getBody();
+        return (String) this.client.put("/_config/"+ section +"/"+ key, null, value).getBody();
     }
 
     public String removeConfig(String section, String key) throws Exception {
