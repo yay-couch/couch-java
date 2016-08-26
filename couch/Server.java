3c9bf0a56f992177cfe0ec4d55963ba3ff9f3f5d
Kerem Güneş <k-gun@mail.com>
2016-08-26 09:16:13 +0300

    Overload getConfig().

--- couch/Server.java
+++ couch/Server.java
@@ -82,6 +82,9 @@ public class Server
        return (202 == this.client.post("/_restart", null, null, null).getStatusCode());
     }
 
+    public JSONObject getConfig(String section) throws Exception {
+        return this.getConfig(section, "");
+    }
     public JSONObject getConfig(String section, String key) throws Exception {
         return this.client.get("/_config/"+ section +"/"+ key, null, null).getBodyData().jsonObject();
     }
