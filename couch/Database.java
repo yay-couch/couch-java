9f71ee48719337a57761d7d93a57cbda6934a504
Kerem Güneş <k-gun@mail.com>
2016-08-26 10:30:20 +0300

    Add couch.Database object.

---
+++ couch/Database.java
@@ -0,0 +1,15 @@
+package couch;
+
+import java.util.Map;
+import java.util.HashMap;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import couch.util.Util;
+
+public class Database
+{
+    private final Client client;
+    private final String name;
+}
