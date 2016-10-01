2bc7a050cac614e1383d4323f478a8d8d9edc55a
Kerem Güneş <k-gun@mail.com>
2016-10-02 00:59:34 +0300

    Add Document object.

---
+++ couch/Document.java
@@ -0,0 +1,22 @@
+package couch;
+
+import java.util.Map;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import couch.util.Util;
+
+public class Document
+{
+    private final Database database;
+
+    public Document(Database database) {
+        this.database = database;
+    }
+
+    public Document(Database database, Object... data) {
+        this.database = database;
+        this.setData(data);
+    }
+}
